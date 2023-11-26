fc22ec953d52f2edbb3ce35e7e94730e0ee056de
lotexiu <alephcostamelo@gmail.com>
2023-11-26 17:23:45 -0300

    novas validaçoes, correções no input e criação utils.

--- src/app/input-v2/input-v2.component.ts
+++ src/app/input-v2/input-v2.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { FormsModule } from '@angular/forms';
 import { Subject, debounceTime } from 'rxjs';
+import { InputUtils } from './utils';
 
 @Component({
     selector: 'input-v2',
@@ -13,7 +14,8 @@ import { Subject, debounceTime } from 'rxjs';
       CommonModule,
       FormsModule,
       NgxMaskDirective,
-      NgxMaskPipe
+      NgxMaskPipe,
+      InputUtils
     ],
     providers: [provideNgxMask()]
 })
@@ -24,8 +26,9 @@ export class InputV2Component implements OnInit, OnChanges{
   @Input() mask = "";
   @Input() type = "text";
   @Input() ngModel:any = "";
+  @Input() debounceTime: number|null = null;
   @Input() decimals = "2";
-  @Input({transform:booleanAttribute}) allowNegative = true;
+  @Input({transform:booleanAttribute}) allowNegative = false;
   @Input({transform:booleanAttribute}) require = false;
   @Input({transform:booleanAttribute}) optional = false;
   @Input({transform:booleanAttribute}) error = false;
@@ -34,8 +37,10 @@ export class InputV2Component implements OnInit, OnChanges{
   genericError = false;
   ngModelMask:any = ""
   errorStatus = ""
+  previousAllowNegative = this.allowNegative
 
   onchange = new Subject<string>();
+  utils = new InputUtils()
   constructor(){
     this.onchange
     .pipe(debounceTime(300)) // valor de debounceTime (tempo que deve esperar apos a acao do usuario no input)
@@ -44,14 +49,29 @@ export class InputV2Component implements OnInit, OnChanges{
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void { // executado a cada mudança externa em @inputs
-    throw new Error('Method not implemented.');
-  }
-
   ngOnInit(): void { // excutado apenas uma vez na chamada do componente. <input-v2>
+    this.type = this.type.toLowerCase()
     this.updateStatus()
     this.setDrawPathIcon()
   }
+
+  ngOnChanges(changes: SimpleChanges): void { // executado a cada mudança externa em @inputs
+    this.onchange
+    .pipe(debounceTime(this.getDebounceTime())) // valor de debounceTime (tempo que deve esperar apos a acao do usuario no input)
+    .subscribe(() => {
+      this.doOnChange();
+    });
+
+    this.type = this.type.toLowerCase()
+    
+    if(this.type == "phone"){
+      this.previousAllowNegative = this.allowNegative
+      this.allowNegative=false
+    }else{
+      this.allowNegative = this.previousAllowNegative
+    }
+  }
+
 
   updateStatus() {
     if(this.error && this.errorMessage != '' && !this.genericError){
@@ -64,15 +84,75 @@ export class InputV2Component implements OnInit, OnChanges{
     }
     else if(this.type == "cpf"){
       this.setErrorStatus(
-        !this.isValidCPF(this.ngModelMask), "CPF"
+        !this.utils.isValidCPF(this.ngModelMask), "CPF"
       )
     }
     else if(this.type == "cnpj"){
       this.setErrorStatus(
-        !this.isValidCNPJ(this.ngModelMask), "CNPJ"
+        !this.utils.isValidCNPJ(this.ngModelMask), "CNPJ"
       )
-    }else{
+    }
+    else if(this.type == "email"){
+      this.setErrorStatus(
+        !this.utils.isValidEmail(this.ngModelMask), "Email"
+      )
+    }
+    else{
       this.setErrorStatus()
+    }
+  }
+
+  doOnChange() {
+    switch(this.type){
+      case "percent":
+        this.ngModel = this.ngModelMask/100
+        break
+      case "phone":
+        this.ngModel = this.utils.onlyDigits(this.ngModelMask)
+        break
+      case "cpf":
+        this.ngModel = this.utils.onlyDigits(this.ngModelMask)
+        break
+      case "cnpj":
+        this.ngModel = this.utils.onlyDigits(this.ngModelMask)
+        break
+      default:
+        this.ngModel = this.ngModelMask
+        break
+    }
+    this.updateStatus()
+  }
+
+  getDebounceTime(){
+    let minDebouce = this.debounceTime && this.debounceTime == 0 ? 100 : this.debounceTime
+    return(
+      minDebouce ? minDebouce :
+      300
+    )
+  }
+
+  getMask(): string {
+    switch(this.type){
+      case "cpf":
+        return "CPF_CNPJ";
+      case "cnpj":
+        return "CPF_CNPJ";
+      case "time":
+        return "Hh:m0:s0"
+      case "date":
+        return "d0/M0/0000"
+      case "ip":
+        return "IP"
+      case "phone":
+        return  '0000-0000||(00) 0000-0000||(00) 00000-0000||+00 (00) 0000-0000||+00 (00) 00000-0000'
+      case "number":
+        return `separator.${this.decimals}`;
+      case "percent":
+        return `separator.${this.decimals}`;
+      case "money":
+        return `separator.${this.decimals}`;
+      default:
+        return this.mask;
     }
   }
 
@@ -92,127 +172,9 @@ export class InputV2Component implements OnInit, OnChanges{
     }
   }
 
-  onChange() {
-    this.onchange.next(`1`)
-  }
-
-  doOnChange() {
-    if(this.type == 'percent'){
-      this.ngModel = this.ngModelMask/100
-    } 
-    else if(this.type == 'phone'){
-      this.ngModelMask = this.formatPhone(this.ngModelMask)
-      this.ngModel = this.onlyDigits(this.ngModelMask)
-    } 
-    else if(this.type == 'cpf'){
-      this.ngModel = this.onlyDigits(this.ngModelMask)
-    } 
-    else if(this.type == 'cnpj'){
-      this.ngModel = this.onlyDigits(this.ngModelMask)
-    } else {
-      this.ngModel = this.ngModelMask
-    }
-    this.updateStatus()
-  }
-
-  onlyDigits(value:string):string{
-    return value.replace(/\D/g, '')
-  }
-
-  formatPhone(phone: string){
-    let phoneOnlyDigits = this.onlyDigits(phone).slice(0,13)
-    let phoneSize = phoneOnlyDigits.length
-
-    function dynamicPhoneMask(){
-      let maskPart = phoneSize > 4 ? "-" : ""
-      return (
-        `${phoneOnlyDigits.slice(0,4)}${maskPart}${phoneOnlyDigits.slice(4,8)}`
-      )
-    }
-
-    return(
-      phoneSize <   9 ? dynamicPhoneMask() :
-      phoneSize ==  9 ? phoneOnlyDigits.replace(/(\d{5})(\d{4})$/, '$1-$2') :
-      phoneSize == 10 ? phoneOnlyDigits.replace(/(\d{2})(\d{4})(\d{4})$/, '($1) $2-$3') :
-      phoneSize == 11 ? phoneOnlyDigits.replace(/(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3') :
-      phoneSize == 12 ? phoneOnlyDigits.replace(/(\d{2})(\d{2})(\d{4})(\d{4})$/, '+$1 ($2) $3-$4') :
-      phoneOnlyDigits.replace(/(\d{2})(\d{2})(\d{5})(\d{4})$/, '+$1 ($2) $3-$4')
-    )
-  }
-
-  isValidCPF(cpf: string): boolean {
-    cpf = cpf.replace(/[\s.-]*/g, '');
-    if (!cpf || cpf.length !== 11 || /^(.)\1+$/.test(cpf)) {
-        return false;
-    }
-    const calculateDigit = (slice: string): number => {
-        let sum = 0;
-        for (let i = 0; i < slice.length; i++) {
-            sum += parseInt(slice[i]) * (slice.length + 1 - i);
-        }
-
-        let remainder = (sum * 10) % 11;
-        if (remainder === 10 || remainder === 11) {
-            remainder = 0;
-        }
-
-        return remainder;
-    };
-
-    const firstDigit = calculateDigit(cpf.slice(0, 9));
-    const secondDigit = calculateDigit(cpf.slice(0, 10));
-
-    return (
-        firstDigit === parseInt(cpf[9]) &&
-        secondDigit === parseInt(cpf[10])
-    );
-  }
-
-  isValidCNPJ(cnpj: string): boolean {
-    const weights: number[] = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-    const digits: string = this.onlyDigits(cnpj);
-    let n = 0;
-
-    for (let i = 0; i < 12; i++) {
-        const digit = parseInt(digits[i], 10);
-        const weight = weights[i + 1];
-        n += digit * weight;
-    }
-    const firstDigit = (((n %= 11) < 2) ? 0 : 11 - n);
-    if (parseInt(digits[12], 10) !== firstDigit) {
-        return false;
-    }
-
-    n = 0;
-    for (let i = 0; i <= 12; i++) {
-        const digit = parseInt(digits[i], 10);
-        const weight = weights[i];
-        n += digit * weight;
-    }
-    const secondDigit = (((n %= 11) < 2) ? 0 : 11 - n);
-    if (parseInt(digits[13], 10) !== secondDigit) {
-        return false;
-    }
-
-    return true;
-  }
-
   setDrawPathIcon() {
     if(this.drawPathIcon == ''){
-      this.drawPathIcon = 
-        "M8 10C8 7.79086 9.79086 6 12 6C14.2091 6 16 7.79086 16 10V11H17C18.933 " +
-        "11 20.5 12.567 20.5 14.5C20.5 16.433 18.933 18 17 18H16C15.4477 18 15 " +
-        "18.4477 15 19C15 19.5523 15.4477 20 16 20H17C20.0376 20 22.5 17.5376 " +
-        "22.5 14.5C22.5 11.7793 20.5245 9.51997 17.9296 9.07824C17.4862 6.20213 " +
-        "15.0003 4 12 4C8.99974 4 6.51381 6.20213 6.07036 9.07824C3.47551 9.51997 " +
-        "1.5 11.7793 1.5 14.5C1.5 17.5376 3.96243 20 7 20H8C8.55228 20 9 19.5523 " +
-        "9 19C9 18.4477 8.55228 18 8 18H7C5.067 18 3.5 16.433 3.5 14.5C3.5 12.567 " +
-        "5.067 11 7 11H8V10ZM15.7071 13.2929L12.7071 10.2929C12.3166 9.90237 " +
-        "11.6834 9.90237 11.2929 10.2929L8.29289 13.2929C7.90237 13.6834 7.90237 " +
-        "14.3166 8.29289 14.7071C8.68342 15.0976 9.31658 15.0976 9.70711 14.7071L11 " +
-        "13.4142V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 " +
-        "19V13.4142L14.2929 14.7071C14.6834 15.0976 15.3166 15.0976 15.7071 " +
-        "14.7071C16.0976 14.3166 16.0976 13.6834 15.7071 13.2929Z"
+      this.drawPathIcon = this.utils.uploadIconPath
       }
-    }
+  }
 }
