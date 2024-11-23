5f71a3766d7cdeb21968c89dcb87eb76ac8f0438
Aleph Costa Melo <alephcostamelo@gmail.com>
2024-11-23 18:41:17 -0300

    Criando um novo Modal

--- src/app/components/lote/modal/lote-modal/lote-modal.service.ts
+++ src/app/components/lote/modal/lote-modal/lote-modal.service.ts
@@ -13,21 +13,35 @@ export class LoteModalService {
 
   open<T>(component: Type<T>, data?: any): ComponentRef<LoteModalComponent> {
     this.overlayRef = this.overlay.create({
-      hasBackdrop: true,
+      hasBackdrop: false, // Alterado de true para false
       positionStrategy: this.overlay
         .position()
         .global()
         .centerHorizontally()
-        .centerVertically()
+        .centerVertically(),
+      scrollStrategy: this.overlay.scrollStrategies.block()
     });
 
     const portal = new ComponentPortal(LoteModalComponent);
     const componentRef = this.overlayRef.attach(portal);
-    
+
+    // Adiciona estilos diretamente ao overlay
+    const overlayElement = this.overlayRef.overlayElement;
+    overlayElement.style.position = 'fixed';
+    overlayElement.style.height = '100%';
+    overlayElement.style.width = '100%';
+
     componentRef.instance.childComponent = component;
     componentRef.instance.data = data;
     componentRef.instance.overlayRef = this.overlayRef;
 
     return componentRef;
+  }
+
+  close(): void {
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
+      this.overlayRef = null;
+    }
   }
 }
