bf8c99dd9507cc1bad8974bb5d930d1340ef16b8
Aleph Costa Melo <alephcostamelo@gmail.com>
2025-04-13 23:15:15 -0300

    Criando interfaces do deep interfaces. Contas matematicas em tipagem

--- src/utils/typescript/extras/interfaces/deep-interface.ts
+++ src/utils/typescript/extras/interfaces/deep-interface.ts
@@ -1,63 +1,68 @@
-﻿import { BuildArray } from "@utils/interfaces/array-interfaces";
-import { _ } from "node_modules/@angular/cdk/number-property.d-1067cb21";
+﻿type INumber<V> = V extends number ? V & number : never;
 
-
-type Number<V> = V extends number ? V & number : never;
-
-type _DArray<
+type _DArray< /* Melhorar */
   Length extends string | number,
   Acc extends any[] = [],
 > = `${Acc['length']}` extends `${Length}` ? Acc : _DArray<Length, [...Acc, any]>;
 
-type _DIsNegative<T extends string> =
-  T extends `-${string}` ? true : false;
+type _DIsNegative<T extends (number|string)> =
+  `${T}` extends `-${infer R}` ? true : false;
   
 type DIsNegative<T extends number> = _DIsNegative<`${T}`>;
 
 type DNegate<T extends (number|string)> =
-  `${T}` extends `-${infer R}` ? Number<R> : Number<`-${T}`>;
+  `${T}` extends `-${infer R}` ? R : `-${T}`;
 
-type DAbs<T extends number> =
+type DAbs<T extends (number|string)> =
   `${T}` extends `-${infer R}` ? R : `${T}`;
 
-type _DAdd<A extends (number|string), B extends (number|string)> =
+type _DAdd<
+  A extends (number|string),
+  B extends (number|string)
+> =
   [..._DArray<A>, ..._DArray<B>]['length']
 ;
 
-type _DSubtract<A extends number, B extends number> =
-  _DArray<A> extends [..._DArray<B>, ...infer Rest]
+type _DSubtract<
+  A extends (number|string),
+  B extends (number|string)
+> =
+  _DArray<DAbs<A>> extends [..._DArray<DAbs<B>>, ...infer Rest]
     ? Rest['length']
     : never
 ;
 
-type _DGreaterThan<A extends number, B extends number> =
-  _DArray<A> extends [..._DArray<B>, ...infer _]
-    ? true
-    : false
+type _DAbsGreaterThan<
+  A extends (number|string),
+  B extends (number|string)
+> = _DArray<DAbs<A>> extends [..._DArray<DAbs<B>>, ...infer _]
+  ? true
+  : false
 ;
 
+type _DAbsDecreaseBiggestValue<
+  A extends (number|string),
+  B extends (number|string)
+> = _DAbsGreaterThan<A, B> extends true ?
+  _DIsNegative<A> extends true ?
+    DNegate<_DSubtract<A, B>> :
+    _DSubtract<A, B> :
+  _DIsNegative<B> extends true ?
+    DNegate<_DSubtract<B, A>> :
+    _DSubtract<B, A>
+;
 
-// _DAdd<DAbs<A>, DAbs<B>> extends infer V ?
-type DMerge<A extends number, B extends number> =
+type _DMerge<A extends number, B extends number> =
   DIsNegative<A> extends true ?
     DIsNegative<B> extends true ?
-      DNegate<Number<_DAdd<DAbs<A>, DAbs<B>>>> :
-    never:
-  never
-      
-  
-  // DIsNegative<A> extends DIsNegative<B>
-  //   ? DIsNegative<A> extends true
-  //     ? DNegate<Number<_DAdd<DAbs<A>, DAbs<B>>>>
-  //     : _DAdd<A, B>
-  //   : _DGreaterThan<Number<DAbs<A>>, Number<DAbs<B>>> extends true
-  //     ? DIsNegative<A> extends true
-  //       ? DNegate<_DSubtract<Number<DAbs<A>>, Number<DAbs<B>>>>
-  //       : _DSubtract<Number<DAbs<A>>, Number<DAbs<B>>>
-  //     : DIsNegative<B> extends true
-  //       ? DNegate<_DSubtract<Number<DAbs<B>>, Number<DAbs<A>>>>
-  //       : _DSubtract<Number<DAbs<B>>, Number<DAbs<A>>>
+      DNegate<INumber<_DAdd<DAbs<A>, DAbs<B>>>> :
+    _DAbsDecreaseBiggestValue<A, B> :
+  DIsNegative<B> extends true ?
+    _DAbsDecreaseBiggestValue<A, B> :
+  INumber<_DAdd<DAbs<A>, DAbs<B>>>
 ;
 
-type A = DMerge<-2,-5>
-      
+type DCalculate<
+  A extends number,
+  B extends number
+> = _DMerge<A,B>
